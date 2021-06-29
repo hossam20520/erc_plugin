2c05e783a885d08e1eed835c0b8c473fef750354
Hossam Hassan <hossamhassan889@gmail.com>
2021-06-29 20:24:44 +0200

    first commit55

--- Inc/Api/CallBacks/AdminCallBacks.php
+++ Inc/Api/CallBacks/AdminCallBacks.php
@@ -10,15 +10,15 @@ use Inc\Base\BaseController;
 class AdminCallBacks extends BaseController{
 
 public function adminDashboard(){
-    return require_once("$this->plugin_path/templates/admin.php");
+    return require_once("$this->plugin_path/Templates/admin.php");
 }
 
 public function projectHomeAdd(){
-    return require_once("$this->plugin_path/templates/projects.php");
+    return require_once("$this->plugin_path/Templates/projects.php");
 }
 
 public function projectInfoAdd(){
-    return require_once("$this->plugin_path/templates/project_info.php");
+    return require_once("$this->plugin_path/Templates/project_info.php");
 }
 
 
